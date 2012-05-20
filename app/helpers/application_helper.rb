db6cf4be453abf04dbbc4f57d0e37a3fca758b8b
Rex He <prohenghzh@gmail.com>
2012-05-20 22:20:50 +1000

    Added a Users controller and layout test link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,18 @@
 module ApplicationHelper
   
-  # def title_help
-  #   
-  #   #Define general page title
-  #   base_title = "Rex Ruby on Rails Learning"
-  #   if(@title.nil?)
-  #     base_title
-  #   else
-  #     base_title = "Rex Ruby on Rails Learning | #{@title}"
-  #   end
-  # end
+  def title_help
+    
+    #Define general page title
+    base_title = "Rex Ruby on Rails Learning"
+    if(@title.nil?)
+      base_title
+    else
+      base_title = "Rex Ruby on Rails Learning | #{@title}"
+    end
+  end
+  
+  def logo
+     image_tag("logo.png", :alt => "Sample App", :class => "round") 
+  end
+  
 end
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+#This is for integration test. Need to test it out base on URL not action in controller_spec
 
 describe "LayoutLinks" do
   # describe "GET /layout_links" do
@@ -29,4 +30,21 @@ describe "LayoutLinks" do
       response.should have_selector 'title' , :content => "Help"
     end
   
+    it "should have a Signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector 'title', :content => "Sign Up"
+    end
+    
+    # Make sure link is working
+    it "should have the right links on the layout" do
+      visit root_path
+      response.should have_selector('title', :content=> "Home")
+      click_link "About"
+      response.should have_selector('title', :content=> "About")
+      click_link "Contact"
+      response.should have_selector('title', :content=>"Contact")
+      # click_link "Sign up now!"
+      #       response.should have_selector('title', :content=>"Sign Up")
+    end
+    
 end
