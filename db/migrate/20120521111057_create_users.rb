bd8965d53ffb0f2e96b8f07da7af853d1b1153f5
Rex He <prohenghzh@gmail.com>
2012-05-21 21:37:56 +1000

    create a user model

---
+++ db/migrate/20120521111057_create_users.rb
@@ -0,0 +1,10 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+
+      t.timestamps #create create_at and updated_at column. 
+    end
+  end
+end
