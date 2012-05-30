60fb0bc66f3e75091960c5f4cd837c566cf42402
Rex He <prohenghzh@gmail.com>
2012-05-30 22:30:28 +1000

    secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 	attr_accessor	:password,:password_confirmation		#normal ruby class attribute not belong to ActiveRecord
 	attr_accessible :email, :name, :password, :password_confirmation
 	#NB: the :password and :password_confirmation are put in attr_accessor for Rspec user_spec.rb testing
-	#NB: there is another field called encrypted_password is not accessible
+	#NB: attr_accessible in rails Active Record http://stackoverflow.com/questions/1789996/attr-accessible-in-rails-active-record
 
 	validates :name, 	:presence => true,
 						:length	  => { :maximum => 50 }
@@ -15,6 +15,45 @@ class User < ActiveRecord::Base
 	validates :password, :presence	=> true,
 						 :confirmation	=> true, #with this field, rails will create a extra field can {field_name}_confirmation. In this case, it created password_confirmation
 					 	 :length => { :in => 6..40 } #with range
+
+	before_save :encrypt_password #the callback method, calling on a method called encrypted_password 
+
+	def self.authenticate email, submitted_password 
+		# or can be call User.authenticate !!!!!! 
+		#self.method_name means the method is class methods not instance methdos. !!!! 
+		#different from self.variable_name is instance variable
+		#alternatively, can put class method as following, class level methods
+		##class << self
+		## def method_name
+		## end
+		##end
+		
+		user = find_by_email email
+		return nil if user.nil?
+		return user if user.has_password? submitted_password
+	end
+
+	def has_password? submitted_password
+		self.encrypted_password == encrypt(submitted_password)
+	end
+
+	private
+		def encrypt_password
+			self.salt 				= make_salt if new_record? #if it is new record
+			self.encrypted_password = encrypt(self.password) # keyword self, call upon the method or variable inside the class 
+		end
+
+		def encrypt string
+			secure_hash "#{self.salt}--#{string}"
+		end
+
+		def secure_hash string
+			Digest::SHA2.hexdigest string
+		end
+
+		def make_salt
+			secure_hash "#{Time.now.utc}--#{self.password}"
+		end
 end
 # == Schema Information
 #
@@ -26,5 +65,6 @@ end
 #  created_at         :datetime        not null
 #  updated_at         :datetime        not null
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
